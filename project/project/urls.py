8d99056f69e5db7ce945f99d93386cc1fb8df6fe
new <75909278+Alexander-050289@users.noreply.github.com>
2021-04-01 14:25:19 +0300

    Added frontent klient vue.js which recieve data from backend server

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from orders.views import orders_page, OrderView
+from orders.views import orders_page, OrderView, orders_app
 from rest_framework.routers import SimpleRouter
 
 router = SimpleRouter()
@@ -23,7 +23,8 @@ router.register('api/orders', OrderView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', orders_page)
+    path('', orders_page),
+    path('orders_page', orders_app),
 ]
 
 urlpatterns += router.urls

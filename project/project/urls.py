60e3c24d609ccb4188103ee00d509f4d8b4e9436
new <75909278+Alexander-050289@users.noreply.github.com>
2021-04-01 11:48:46 +0300

    Added REST API and created serializers.py. Site work correctly

--- project/project/urls.py
+++ project/project/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from orders.views import orders_page, OrderView
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('api/orders', OrderView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', orders_page)
 ]
+
+urlpatterns += router.urls
